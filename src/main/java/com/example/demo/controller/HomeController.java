5518e04f27680dfcab1d4d2ef0739e7adbafb731
meetdhameliya <88376029+meetdhameliya@users.noreply.github.com>
2023-04-13 12:12:30 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, World";
+        return "you are in india नमस्ते";
     }
 
 }
